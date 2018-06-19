00f6d2257a3593ee4d4ffcd46bb77dab717afe49
shigesan1019 <testament1019@gmail.com>
2018-06-20 00:49:48 +0900

    筒の中

--- code.py
+++ code.py
@@ -1,10 +1,20 @@
-input_lines = input()
-input_lifes = input()
-sum = 0
+input_num = input()
+input_q = input()
+code = ''
+met = ''
+pl = ''
 
-for char in input_lines:
-  sum  += int(char)
-if sum >= int(input_lifes):
-  print('OK')
-else:
-  print('NG')
+for i in range(int(input_num)):
+  met = str(i + 1)
+  if input_q[i] == 'L':
+    code = met + code
+  elif input_q[i] == 'R':
+    code = code + met
+codes = list(code)
+
+for char in codes:
+  pl += char + ' '
+
+pl = pl.rstrip(' ') + '\r'
+
+print(pl)
